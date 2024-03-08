ce0edcb3744c54c4a489551d2616949c99e3652c
Talari Praveen Kumar <116370782+code-with-praveen143@users.noreply.github.com>
2024-03-08 20:59:52 +0530

    Update index.js

--- src/components/CashWithdrawal/index.js
+++ src/components/CashWithdrawal/index.js
@@ -16,7 +16,7 @@ class CashWithdrawal extends Component {
   render() {
     const {denominationsList} = this.props
     const {balance} = this.state
-    const name = 'Sarah Williams'
+    const name = 'Praveen Kumar'
     const initial = name.slice(0, 1)
 
     return (
